7ed0b03549addd3a5d8ce52416473748024d2138
giscardAsan <asantegiscard23@gmail.com>
2024-09-15 05:18:58 +0000

    commit

--- scritp/btn.js
+++ scritp/btn.js
@@ -192,7 +192,13 @@
     });
 
 
-
+    function toggle(){
+        var trailer = document.querySelector(".trailer");
+        var video = document.querySelector("video");
+        trailer.classList.toggle("active");
+        video.pause();
+        video.currentTime = 0;
+      }
 
 
 
